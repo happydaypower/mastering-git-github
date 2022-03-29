b561ac819c5bbb27deddeeb21a7503007e6cc1bf
happydaypower <kwj001@paran.com>
2022-03-29 10:48:21 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,12 @@
+const { describe } = require("mocha");
+
+describe('Default Test Set', () => {
+if('test1 should be passed', () => {
+    console.log('test1 passed')
+});
+
+if('test2 should be passed', () => {
+    console.log('test2 passed')
+});
+
+});
